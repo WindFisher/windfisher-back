73d8627596156fcff014bf1076393e4f2bdc6567
WindFisher <windfisher@outlook.com>
2022-02-12 17:23:12 +0100

    Fixed broken install plugin

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   url: env("https://windfisher.herokuapp.com/"),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+    keys: env.array("APP_KEYS", []),
   },
 });
--- config/middlewares.js
+++ config/middlewares.js
@@ -8,7 +8,7 @@ module.exports = [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  "strapi::security",
+  //"strapi::security",
   {
     name: "strapi::security",
     config: {
